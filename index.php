499d2f5c3553965b6be5071e63d0dc34ef98df64
kavichen <chen1qi2wei3@gmail.com>
2013-09-06 14:34:41 +0800

    index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * wechat php test
- */
+  * wechat php test
+  */
 
 //define your token
 define("TOKEN", "kavichen");
 $wechatObj = new wechatCallbackapiTest();
+$wechatObj->responseMsg();
 $wechatObj->valid();
 
 class wechatCallbackapiTest
@@ -28,33 +29,123 @@ class wechatCallbackapiTest
 
         //extract post data
         if (!empty($postStr)){
+                
+                $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $RX_TYPE = trim($postObj->MsgType);
 
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $time = time();
-            $textTpl = "<xml>
-                <ToUserName><![CDATA[%s]]></ToUserName>
-                <FromUserName><![CDATA[%s]]></FromUserName>
-                <CreateTime>%s</CreateTime>
-                <MsgType><![CDATA[%s]]></MsgType>
-                <Content><![CDATA[%s]]></Content>
-                <FuncFlag>0</FuncFlag>
-                </xml>";             
-            if(!empty( $keyword ))
-            {
-                $msgType = "text";
-                $contentStr = "Welcome to wechat world!";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                switch($RX_TYPE)
+                {
+                    case "text":
+                        $resultStr = $this->handleText($postObj);
+                        break;
+                    case "event":
+                        $resultStr = $this->handleEvent($postObj);
+                        break;
+                    default:
+                        $resultStr = "Unknow msg type: ".$RX_TYPE;
+                        break;
+                }
                 echo $resultStr;
-            }else{
-                echo "Input something...";
-            }
-
         }else {
             echo "";
             exit;
+        }
+    }
+
+    public function handleText($postObj)
+    {
+        $fromUsername = $postObj->FromUserName;
+        $toUsername = $postObj->ToUserName;
+        $keyword = trim($postObj->Content);
+        $time = time();
+        $textTpl = "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[%s]]></MsgType>
+                    <Content><![CDATA[%s]]></Content>
+                    <FuncFlag>0</FuncFlag>
+                    </xml>";             
+        if(!empty( $keyword ))
+        {
+            // $msgType = "text";
+            // $contentStr = "陈琦威：".
+            //                 "\n".
+            //                 "平台目前处于开发阶段，我可以收到你发过来的信息，但是不能处理，请见谅！";
+            $msgType = "text";
+
+            $str = mb_substr($keyword, -2, 2,"UTF-8");
+            $str_key = mb_substr($keyword, 0, -2,"UTF-8");
+            if($str == '天气' && !empty($str)){
+                $data = $this->weather($str_key);
+                if(empty($data->weatherinfo)){
+                    $contentStr = "干，没有查到\"".$str_key."\"的天气信息！";
+                }
+                else
+                {
+                    $contentStr = "【".$data->weatherinfo->city."天气预报】\n".$data->weatherinfo->date_y." ".$data->weatherinfo->fchh."时发布"."\n\n实时天气\n".$data->weatherinfo->weather1." ".$data->weatherinfo->temp1." ".$data->weatherinfo->wind1."\n\n温馨提示：".$data->weatherinfo->index_d."\n\n明天\n".$data->weatherinfo->weather2." ".$data->weatherinfo->temp2." ".$data->weatherinfo->wind2."\n\n后天\n".$data->weatherinfo->weather3." ".$data->weatherinfo->temp3." ".$data->weatherinfo->wind3;
+                }
+            }
+            else
+            {
+            $msgType = "text";
+            $contentStr = "陈琦威：".
+                            "\n".
+                            "平台目前处于开发阶段，我可以收到你发过来的信息，但是不能处理，请见谅！";
+            }
+            $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+            echo $resultStr;
+        }else{
+            echo "Input something...";
+        }
+    }
+
+    public function handleEvent($object)
+    {
+        $contentStr = "";
+        switch ($object->Event)
+        {
+            case "subscribe":
+                $contentStr = "感谢您关注C's A.T.Field".
+                                "\n".
+                                "\n".
+                                "微信号：MyAtField (不分大小写)".
+                                "\n".
+                                "\n".
+                                "更多内容，敬请期待...";
+                break;
+            default :
+                $contentStr = "Unknow Event: ".$object->Event;
+                break;
+        }
+        $resultStr = $this->responseText($object, $contentStr);
+        return $resultStr;
+    }
+    
+    public function responseText($object, $content, $flag=0)
+    {
+        $textTpl = "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[text]]></MsgType>
+                    <Content><![CDATA[%s]]></Content>
+                    <FuncFlag>%d</FuncFlag>
+                    </xml>";
+        $resultStr = sprintf($textTpl, $object->FromUserName, $object->ToUserName, time(), $content, $flag);
+        return $resultStr;
+    }
+
+    private function weather($n){
+        include("weather_cityId.php");
+        $c_name = $weather_cityId[$n];
+        if(!empty($c_name)){
+            $json=file_get_contents("http://m.weather.com.cn/data/".$c_name.".html");
+            return json_decode($json);
+        }
+        else
+        {
+            return $c_name;
         }
     }
 
@@ -62,14 +153,14 @@ class wechatCallbackapiTest
     {
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];	
-
+        $nonce = $_GET["nonce"];    
+                
         $token = TOKEN;
         $tmpArr = array($token, $timestamp, $nonce);
         sort($tmpArr);
         $tmpStr = implode( $tmpArr );
         $tmpStr = sha1( $tmpStr );
-
+        
         if( $tmpStr == $signature ){
             return true;
         }else{
