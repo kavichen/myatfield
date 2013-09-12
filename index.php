f4f7c8402fb031c2f8868a61fe3322ca8fae1fd5
kavichen <chen1qi2wei3@gmail.com>
2013-09-12 16:21:22 +0800

    dingyue

--- index.php
+++ index.php
@@ -96,9 +96,9 @@ class wechatCallback
                 <Articles>\n";
                 $resultStr .="<item>\n
                 <Title><![CDATA[test]]></Title>\n
-                <Description><![CDDTA[]]></Description>\n
+                <Description><![CDDTA[test]]></Description>\n
                 <PicUrl><![CDATA[http://chenqiwei.com/profile/8bit.jpg]]</PicUrl>\n
-                <Url><![CDATA[http://chenqiwei.com]]</Url>\n
+                <Url><![CDATA[http://chenqiwei.com]]></Url>\n
                 </item>\n";
                 /*
                  *$resultStr .= "<item>\n <Title><![CDATA[test2]]></Title>\n <Description><![CDATA[]]></Description>\n <PicUrl><![CDATA[http://chenqiwei.com/profile/8bit.jpg]]</PicUrl>\n <Url><![CDATA[http://chenqiwei.com]]</Url>\n </item>\n";
